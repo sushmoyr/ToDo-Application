5ab7ba4210673867dd641273eaadf335a6df82ea
Sushmoy Roy <34061341+Ex-Mach1na@users.noreply.github.com>
2021-02-16 00:33:56 +0600

    safe args, update Fragment

--- app/src/main/java/com/sushmoyr/todoapplication/fragments/ListAdapter.kt
+++ app/src/main/java/com/sushmoyr/todoapplication/fragments/ListAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.sushmoyr.todoapplication.R
 import com.sushmoyr.todoapplication.data.model.Priority
 import com.sushmoyr.todoapplication.data.model.ToDoData
+import com.sushmoyr.todoapplication.fragments.list.ListFragmentDirections
 import kotlinx.android.synthetic.main.custom_row_layout.view.*
 
 class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
@@ -24,7 +26,10 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemView.title_text.text = dataList[position].title
         holder.itemView.description_text.text = dataList[position].description
-
+        holder.itemView.row_background.setOnClickListener{
+            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(dataList[position])
+            holder.itemView.findNavController().navigate(action)
+        }
         val priority = dataList[position].priority
         when (dataList[position].priority){
             Priority.HIGH -> holder.itemView.indicator.setCardBackgroundColor(
--- app/src/main/java/com/sushmoyr/todoapplication/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/sushmoyr/todoapplication/fragments/update/UpdateFragment.kt
@@ -3,21 +3,47 @@ package com.sushmoyr.todoapplication.fragments.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.sushmoyr.todoapplication.R
+import com.sushmoyr.todoapplication.data.model.Priority
+import com.sushmoyr.todoapplication.data.viewmodel.ToDoViewModel
+import com.sushmoyr.todoapplication.fragments.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_update.*
+import kotlinx.android.synthetic.main.fragment_update.view.*
 
 class UpdateFragment : Fragment() {
+
+    private val args by navArgs<UpdateFragmentArgs>()
+    private val toDoViewModel: ToDoViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_update, container, false)
+
+        view.current_title_et.setText(args.currentItem.title)
+        view.current_description_et.setText(args.currentItem.description)
+        view.current_priorities_spinner.setSelection(parsePriority(args.currentItem.priority))
+        view.current_priorities_spinner.onItemSelectedListener = sharedViewModel.listener
+
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_menu, menu)
+    }
+
+    private fun parsePriority(priority: Priority): Int {
+        return when (priority) {
+            Priority.HIGH -> 0
+            Priority.MEDIUM -> 1
+            Priority.LOW -> 2
+        }
     }
 
 }
