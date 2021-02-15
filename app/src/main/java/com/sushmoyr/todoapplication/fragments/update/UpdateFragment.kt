809dc54643332f7615e77cecc7256d12d5ed23ae
Sushmoy Roy <34061341+Ex-Mach1na@users.noreply.github.com>
2021-02-16 00:44:52 +0600

    update to db

--- app/src/main/java/com/sushmoyr/todoapplication/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/sushmoyr/todoapplication/fragments/update/UpdateFragment.kt
@@ -2,11 +2,14 @@ package com.sushmoyr.todoapplication.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.sushmoyr.todoapplication.R
 import com.sushmoyr.todoapplication.data.model.Priority
+import com.sushmoyr.todoapplication.data.model.ToDoData
 import com.sushmoyr.todoapplication.data.viewmodel.ToDoViewModel
 import com.sushmoyr.todoapplication.fragments.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_update.*
@@ -37,6 +40,40 @@ class UpdateFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_save -> {
+                updateDataInDb()
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateDataInDb() {
+        val mTitle = current_title_et.text.toString()
+        val mSpinner = current_priorities_spinner.selectedItem.toString()
+        val mDesc = current_description_et.text.toString()
+
+        val validation = sharedViewModel.verifyData(mTitle, mDesc)
+
+        if(validation){
+            val toDoData = ToDoData(args.currentItem.id,
+                mTitle,
+                sharedViewModel.parsePriority(mSpinner),
+                mDesc)
+
+            toDoViewModel.updateData(toDoData)
+            Toast.makeText(requireContext(), "Updated", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        else{
+            Toast.makeText(requireContext(), "Please Fill All", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     private fun parsePriority(priority: Priority): Int {
         return when (priority) {
